cb9bbe8053b31662ef9d8635b1ec067afe6c9de1
s-kngstn <s.kngstn@protonmail.com>
2022-06-27 13:54:31 +0100

    created api hooks to get verified users restaurant data, now rendering rest data to page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,18 +1,28 @@
 import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { useRestaurants } from "../../lib/hooks";
 import AddCard from "../../src/components/cards/addCard";
 import RestaurantCard from "../../src/components/cards/restaurantCard";
 import SidebarWithHeader from "../../src/components/nav-sidebar/sidebarWithNav";
 
 const Dashboard = () => {
+  const { restaurants } = useRestaurants();
+
+  console.log(restaurants);
+
+  // console.log(user);
   return (
     <SidebarWithHeader>
       <Box marginTop="5rem" sx={{ "--my-color": "#0075bf" }}>
         <Heading color="blackAlpha.900" fontSize="5xl" fontWeight="extrabold">
-          Welcome user, here are your restaurants.
+          Welcome here are your restaurants.
         </Heading>
         <Flex flexDirection="row" flexWrap="wrap">
           <AddCard />
-          <RestaurantCard />
+          {restaurants.map((restaurant: any) => {
+            return (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            );
+          })}
         </Flex>
       </Box>
     </SidebarWithHeader>
