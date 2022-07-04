f475f51c75556a8a788a2ce3ea87ef60e89911eb
s-kngstn <s.kngstn@protonmail.com>
2022-07-04 13:05:13 +0100

    created menu filtering system for menu page

---
+++ lib/allergens.ts
@@ -0,0 +1,61 @@
+export const allergens: {
+  name: string;
+  key: string;
+}[] = [
+  {
+    name: "dairy",
+    key: "Dairy",
+  },
+  {
+    name: "gluten",
+    key: "Gluten",
+  },
+  {
+    name: "nuts",
+    key: "Nuts",
+  },
+  {
+    name: "peanuts",
+    key: "Peanuts",
+  },
+  {
+    name: "sesame",
+    key: "Sesame Seeds",
+  },
+  {
+    name: "soya",
+    key: "Soya",
+  },
+  {
+    name: "sulphites",
+    key: "Sulphur Dioxide",
+  },
+  {
+    name: "eggs",
+    key: "Eggs",
+  },
+  {
+    name: "lupin",
+    key: "Lupin",
+  },
+  {
+    name: "crustacean",
+    key: "Crustacean",
+  },
+  {
+    name: "molluscs",
+    key: "Molluscs",
+  },
+  {
+    name: "mustard",
+    key: "Mustard",
+  },
+  {
+    name: "celery",
+    key: "Celery",
+  },
+  {
+    name: "fish",
+    key: "Fish",
+  },
+];
